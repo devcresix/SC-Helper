488d11902e04396011c7ce33387969da622b5cff
Dev Cresix <52518140+devcresix@users.noreply.github.com>
2023-11-08 01:15:07 +0530

    Initial commit from create-wagmi

---
+++ src/wagmi.ts
@@ -0,0 +1,23 @@
+import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+
+export const walletConnectProjectId = 'fe69a54abe37e7698421d53a4cea84f7'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [w3mProvider({ projectId: walletConnectProjectId })],
+)
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({
+    chains,
+    projectId: walletConnectProjectId,
+    version: 2,
+  }),
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }
